f6ea8018ab72291dae4bc13f285e122cae56716b
Nelisa <nelisa.dludla@outlook.com>
2024-05-15 00:45:10 +0200

    Updated 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-'''
-This function lists all documents in a collection
-'''
-
+'''This function lists all documents in a collection'''
 from pymongo.collection import Collection
 from typing import List
 
 
 def list_all(mongo_collection: Collection) -> List[dict]:
-    '''
-    Returns a list of documents in a collection
-    '''
+    '''Returns a list of documents in a collection'''
 
     documents = mongo_collection.find()
     documents_list = list(documents)
