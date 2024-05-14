833d6bf9d3c79550de7a90c2a79fd77408755cc7
Nelisa <nelisa.dludla@outlook.com>
2024-05-15 00:58:52 +0200

    Updated 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,22 @@ about Nginx logs stored in MongoDB
 
 import pymongo
 
-client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client['logs']
-collection = db['nginx']
+if __name__ == '__main__':
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
 
-total_logs = collection.count_documents({})
+    total_logs = collection.count_documents({})
 
-print(f'{total_logs} logs')
+    print(f'{total_logs} logs')
 
-print('Methods:')
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-for method in methods:
-    method_query = {'method': method}
-    method_count = collection.count_documents(method_query)
-    print(f'\tmethod {method}: {method_count}')
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        method_query = {'method': method}
+        method_count = collection.count_documents(method_query)
+        print(f'\tmethod {method}: {method_count}')
 
-status_query = {'method': 'GET', 'path': '/status'}
-status_count = collection.count_documents(status_query)
-print(f'{status_count} status check')
+    status_query = {'method': 'GET', 'path': '/status'}
+    status_count = collection.count_documents(status_query)
+    print(f'{status_count} status check')
